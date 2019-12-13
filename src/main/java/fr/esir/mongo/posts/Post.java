dcdea9cb780f1feb03b311ba89aaa33d962d2204
jeannoelaristide dago <jdago@e103m07.istic.univ-rennes1.fr>
2019-12-13 14:36:16 +0100

    First model

---
+++ src/main/java/fr/esir/mongo/posts/Post.java
@@ -0,0 +1,32 @@
+package fr.esir.mongo.posts;
+
+import fr.esir.mongo.threads.Thread;
+import fr.esir.mongo.users.User;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+/**
+ *
+ * @author lboutros
+ */
+@Data
+@AllArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@ToString
+@Builder
+public class Post {
+  @EqualsAndHashCode.Include
+  private final String _id;
+  
+  private final String title;
+  
+  private final String content;
+
+  private User user;
+
+  private Thread thread;
+
+}
